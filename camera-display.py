8d9644a0913545cf9d04597098afe12d44431a3f
Kaloyan Kolev <42871424+kaloyaan@users.noreply.github.com>
2023-03-27 21:57:24 -0400

    Added filesystem, restructured code

--- camera-display.py
+++ camera-display.py
@@ -1,95 +1,137 @@
 #!/usr/bin/env python3
 
+import os
 import sys
-
-from PIL import Image
-
 from inky.auto import auto
-
 from io import BytesIO
 from time import sleep
 from picamera import PiCamera
 from PIL import Image
-
+from PIL import UnidentifiedImageError
 import signal
 import RPi.GPIO as GPIO
-
 from inky.inky_uc8159 import CLEAN
 
-BUTTONS = [5, 6, 16, 24]
-LABELS = ['A', 'B', 'C', 'D']
+image_files = []
+current_index = -1
 
+def clear_display():
+    for _ in range(2):
+        for y in range(inky.height - 1):
+            for x in range(inky.width - 1):
+                inky.set_pixel(x, y, CLEAN)
 
-# Gpio pins for each button (from top to bottom)
-BUTTONS = [5, 6, 16, 24]
+        inky.show()
+        sleep(5)
 
-# These correspond to buttons A, B, C and D respectively
-LABELS = ['A', 'B', 'C', 'D']
+def display_image(path):
+    try:
+        image = Image.open(path)
+    except UnidentifiedImageError:
+        print("Can't open image")
+        clear_display()
+        return
+    #Display image
+    saturation = 0.7
+    inky.set_image(image, saturation=saturation)
+    inky.show()
 
-# Set up RPi.GPIO with the "BCM" numbering scheme
-GPIO.setmode(GPIO.BCM)
+def get_image_files():
+    global image_files, current_index
+    if not os.path.exists("images"):
+        os.makedirs("images")
+    image_files = [os.path.join("images", f) for f in os.listdir("images") if f.endswith(".jpg")]
+    image_files.sort()
+    if len(image_files) > 0:
+        current_index = len(image_files) - 1
+        display_image(image_files[current_index])
 
-# Buttons connect to ground when pressed, so we should set them up
-# with a "PULL UP", which weakly pulls the input signal to 3.3V.
-GPIO.setup(BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+def show_prev_image():
+    global current_index
+    if current_index > 0:
+        current_index -= 1
+        display_image(image_files[current_index])
 
-inky = auto(ask_user=True, verbose=True)
+def show_next_image():
+    global current_index
+    if current_index < len(image_files) - 1:
+        current_index += 1
+        display_image(image_files[current_index])
+
+def delete_current_image():
+    global image_files, current_index
+    if len(image_files) == 0:
+        print("No images to delete.")
+        return
+    os.remove(image_files[current_index])
+    del image_files[current_index]
+    if current_index == len(image_files):
+        current_index -= 1
+    if len(image_files) > 0:
+        display_image(image_files[current_index])
+    else:
+        clear_display()
+
+def save_image(image):
+    global image_files, current_index
+    current_index = len(image_files)
+    image_path = "image" + str(current_index) + ".jpg"
+    image_files.append(image_path)
+    image.save(image_path, 'JPEG')
+    print(f"Saved image to {image_path}")
+    display_image(image_path)
+
+def take_photo():
+    #Capture image 
+    stream = BytesIO()
+    camera = PiCamera()
+    camera.resolution = (600, 448)
+    sleep(3)
+    camera.capture(stream, format='jpeg')
+    #'Rewind' stream
+    stream.seek(0)
+    image = Image.open(stream)
+    camera.close()
+    #Save image
+    save_image(image)
 
 # "handle_button" will be called every time a button is pressed
 # It receives one argument: the associated input pin.
-def handle_button(pin):
+def handle_button(pin, image_files, ):
     if pin == 16:
-        print ("clearing")
-        for _ in range(2):
-            for y in range(inky.height - 1):
-                for x in range(inky.width - 1):
-                    inky.set_pixel(x, y, CLEAN)
+        print ("delete current image")
 
-            inky.show()
-            sleep(5)
     if pin == 24:
         print("photo time")
-                
-        #Create memory stream
-        stream = BytesIO()
-        camera = PiCamera()
-        camera.resolution = (600, 448)
-        sleep(3)
-        camera.capture(stream, format='jpeg')
-        #'Rewind' stream
-        stream.seek(0)
-        image = Image.open(stream)
-        
-        camera.close()
+        take_photo()
+    if pin == 6:
+        print("next photo")
+        show_next_image()
+    if pin == 5:
+        print("previous photo")
+        show_prev_image()
 
-        saturation = 0.7
+def main():
+    #Button config
+    BUTTONS = [5, 6, 16, 24] # Gpio pins for each button (from top to bottom)
+    LABELS = ['A', 'B', 'C', 'D'] # These correspond to buttons A, B, C and D respectively
+    GPIO.setmode(GPIO.BCM) # Set up RPi.GPIO with the "BCM" numbering scheme
+    # Buttons connect to ground when pressed, so we should set them up
+    # with a "PULL UP", which weakly pulls the input signal to 3.3V.
+    GPIO.setup(BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-        '''
-        if len(sys.argv) == 1:
-            print("""
-        Usage: {file} image-file
-        """.format(file=sys.argv[0]))
-            sys.exit(1)
+    #Inky settings
+    inky = auto(ask_user=True, verbose=True)
+    saturation = 0.7
 
-        image = Image.open(sys.argv[1])
-        '''
-        #resizedimage = image.resize(inky.resolution)
+    #Get current filesystem, last current_index
+    get_image_files()
+    current_index = 0
 
-        if len(sys.argv) > 2:
-            saturation = float(sys.argv[2])
+    #Add button event detect
+    for pin in BUTTONS:
+        GPIO.add_event_detect(pin, GPIO.FALLING, handle_button, bouncetime=250)
+    signal.pause()
 
-        inky.set_image(image, saturation=saturation)
-        inky.show()
-
-
-
-# Loop through out buttons and attach the "handle_button" function to each
-# We're watching the "FALLING" edge (transition from 3.3V to Ground) and
-# picking a generous bouncetime of 250ms to smooth out button presses.
-for pin in BUTTONS:
-    GPIO.add_event_detect(pin, GPIO.FALLING, handle_button, bouncetime=250)
-
-# Finally, since button handlers don't require a "while True" loop,
-# we pause the script to prevent it exiting immediately.
-signal.pause()
-
+if __name__ == "__main__":
+    main()
